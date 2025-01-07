118b2c2105ff0fd3b15d85f52232c6eaad771937
SM2111 <dinoraj.pattabiraman@solverminds.sg>
2025-01-07 10:16:18 +0530

    initial commit

---
+++ apps/shell/src/app/container/container.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shell-angular-container',
+  templateUrl: './container.component.html',
+  styleUrl: './container.component.scss',
+})
+export class ContainerComponent {
+  isSidebarOpen = true;
+
+}
+
+
